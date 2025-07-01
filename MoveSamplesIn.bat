2f941fa8cd774ee60a3227c0badd658708a9a79e
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-01 21:25:46 +0900

    removed Sample and Test

--- MoveSamplesIn.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Moving samples from project to package.
-REM Clean out what was in there
-rmdir /S /Q com.unity.ads.ios-support\Samples
-
-REM Copy over from the Samples folder in the unity project
-xcopy /S /I /Q SampleProject~\Assets\Samples com.unity.ads.ios-support\Samples
--- MoveSamplesOut.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Moving samples out of the package directory.
-
-REM Clean out what was in there
-rmdir /S /Q com.unity.ads.ios-support\Samples
